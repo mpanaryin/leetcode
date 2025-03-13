f538a525b1b4fbdec11264ee3b88e0b401650bda
DESKTOP-5415H2I\thebe <thebestuzl93@gmail.com>
2025-03-13 11:24:52 +0300

    3356 - hard (diff arr + Line Sweep), 1295, 485, 977

---
+++ 1295 find_numbers_with_even_number_of_digits.py
@@ -0,0 +1,16 @@
+class Solution:
+    """
+    Given an array nums of integers, return how many of them contain an even number of digits.
+
+    Constraints:
+    1 <= nums.length <= 500
+    1 <= nums[i] <= 10^5
+    """
+    def findNumbers(self, nums: list[int]) -> int:
+        return len([num for num in nums if len(str(num)) % 2 == 0])
+
+
+nums = [12, 345, 2, 6, 7896]
+s = Solution()
+result = s.findNumbers(nums)
+print('result', result)
