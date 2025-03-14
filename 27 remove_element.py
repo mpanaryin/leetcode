931f25f9c7ad4dbc6f349f28d3eaadacdceedbb1
DESKTOP-5415H2I\thebe <thebestuzl93@gmail.com>
2025-03-14 20:28:47 +0300

    Разобрана задача

--- 27 remove_element.py
+++ 27 remove_element.py
@@ -14,7 +14,8 @@ class Solution:
         The remaining elements of nums are not important as well as the size of nums.
         Return k.
 
-        IMPORTANT: тут важно смотреть за передаваемым nums, изменяем только его, так как это глобальная вещь
+        IMPORTANT: Мы не удаляем элементы, нам вообще всё равно на них порядок, главное что будет в начале.
+        Пример: nums = [1], val = 1. По итогу мы возвращаем k = 0, а список так и остаётся [1], он не становится пустым
         """
         # Minimal code
         while val in nums:
@@ -33,12 +34,15 @@ class Solution:
 
     def removeElement3(self, nums: List[int], val: int) -> int:
         """FASTEST"""
-        index = 0
+        k = 0
         for i in range(len(nums)):
+            # Если текущее число в массиве не равно нашему числу
             if nums[i] != val:
-                nums[index] = nums[i]
-                index += 1
-        return index
+                # Делаем перестановку. Если до этого не было val, то замены вообще не будет, но если было,
+                # то k будет опаздывать и находится как раз на позиции val, на неё встанет наша замена.
+                nums[k] = nums[i]
+                k += 1
+        return k
 
 
 s = Solution()
