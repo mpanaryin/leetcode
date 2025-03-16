617faf6968775fbffbfe41cce5a6b8faf37d1513
DESKTOP-5415H2I\thebe <thebestuzl93@gmail.com>
2025-03-16 11:57:29 +0300

    283[easy] - arrays/2pointers 1299[easy] - arrays/backwards

---
+++ 1299 _replace_elements_with_greatest_element_on_right_side.py
@@ -0,0 +1,28 @@
+class Solution:
+    """
+    Grade [Easy]
+
+    Given an array arr, replace every element in that array with the greatest element
+    among the elements to its right, and replace the last element with -1.
+
+    After doing so, return the array.
+
+    Constraints:
+    1 <= arr.length <= 10^4
+    1 <= arr[i] <= 10^5
+    """
+    def replaceElements(self, arr: list[int]) -> list[int]:
+        maximum = arr[-1]
+        for i in range(len(arr) - 1, -1, -1):
+            current = arr[i]
+            arr[i] = maximum
+            maximum = max(maximum, current)
+        arr[-1] = -1
+        return arr
+
+
+arr = [17, 18, 5, 4, 6, 1]
+s = Solution()
+result = s.replaceElements(arr)
+print(result)
+print(arr)
