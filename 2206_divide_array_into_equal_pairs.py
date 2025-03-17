833c559c423ab5413ea13475bedc1311f479fa1c
DESKTOP-5415H2I\thebe <thebestuzl93@gmail.com>
2025-03-17 09:01:04 +0300

    2206[easy] - arrays

---
+++ 2206_divide_array_into_equal_pairs.py
@@ -0,0 +1,39 @@
+from collections import Counter
+
+
+class Solution:
+    """
+    Grade [Easy]
+
+    You are given an integer array nums consisting of 2 * n integers.
+
+    You need to divide nums into n pairs such that:
+
+    Each element belongs to exactly one pair.
+    The elements present in a pair are equal.
+    Return true if nums can be divided into n pairs, otherwise return false.
+
+    Constraints:
+    nums.length == 2 * n
+    1 <= n <= 500
+    1 <= nums[i] <= 500
+    """
+
+    def divideArray(self, nums: list[int]) -> bool:
+        """
+        Time Complexity: O(n) - проходимся 2 раза, первый раз по массиву заполняя dict, второй по dict
+        Space Complexity: O(n) - нужно где-то хранить dict с элементами
+        """
+        counter = Counter(nums)
+        for key, value in counter.items():
+            if value % 2 != 0:
+                return False
+        return True
+
+
+nums = [3, 2, 3, 2, 2, 2]
+nums = [1, 2, 3, 4]
+s = Solution()
+result = s.divideArray(nums)
+print(result)
+print(nums)
--- code_templates/backtracking.py
+++ code_templates/backtracking.py
@@ -1,13 +1,13 @@
 BASE_CASE = object
 
-def backtrack(curr, OTHER_ARGUMENTS):
-    if (BASE_CASE):
-        # modify the answer
-        return
-
-    ans = 0
-    for (ITERATE_OVER_INPUT):
-        # modify the current state
-        ans += backtrack(curr, OTHER_ARGUMENTS)
-        # undo the modification of the current state
-    return ans
+# def backtrack(curr, OTHER_ARGUMENTS):
+#     if (BASE_CASE):
+#         # modify the answer
+#         return
+#
+#     ans = 0
+#     for (ITERATE_OVER_INPUT):
+#         # modify the current state
+#         ans += backtrack(curr, OTHER_ARGUMENTS)
+#         # undo the modification of the current state
+#     return ans
