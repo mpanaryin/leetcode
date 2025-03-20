50e5bbd329a9cb6191e4a3ebf028612705faa87b
DESKTOP-5415H2I\thebe <thebestuzl93@gmail.com>
2025-03-20 11:11:32 +0300

    upd 75[medium] - arrays, sort: quicksort, 3 pointers, selection sort

--- 75 sort_colors.py
+++ 75 sort_colors.py
@@ -2,21 +2,34 @@ from typing import List
 
 
 class Solution:
-    def sortColors(self, nums: List[int]) -> None:
+    """
+    Grade [Medium]
+
+    Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the
+    same color are adjacent, with the colors in the order red, white, and blue.
+
+    We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+    You must solve this problem without using the library's sort function.
+
+    Constraints:
+    n == nums.length
+    1 <= n <= 300
+    nums[i] is either 0, 1, or 2.
+
+    Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+    """
+
+    def sortColors(self, nums: list[int]) -> None:
         """
-        Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the
-        same color are adjacent, with the colors in the order red, white, and blue.
-
-        We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
-
-        You must solve this problem without using the library's sort function.
-
-        Do not return anything, modify nums in-place instead.
+        Time complexity: O(n log n)
+        Space complexity: O(1)
         """
+
         def help_func(nums, start, end):
             # Берем средний элемент из уже отрубленного
             # массива или средний из начального
-            q = nums[(start+end) // 2]
+            q = nums[(start + end) // 2]
             # Эти и дальнейшие +/- 1 защищают нас от
             # бесконечного цикла при дубликатах
             i = start - 1
@@ -41,20 +54,62 @@ class Solution:
                     return j
 
                 nums[i], nums[j] = nums[j], nums[i]
+
         def quick_sort(nums, start, end):
             if start < end:
                 p = help_func(nums, start, end)
                 quick_sort(nums, start, p)
                 quick_sort(nums, p + 1, end)
+
         quick_sort(nums, 0, len(nums) - 1)
 
+    def sortColors(self, nums: list[int]) -> None:
+        """
+        Time complexity: O(n^2)
+        Space complexity: O(1)
+        """
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] > nums[j]:
+                    nums[i], nums[j] = nums[j], nums[i]
+
+    def sortColors(self, nums: list[int]) -> None:
+        """
+        Time complexity: O(n^2)
+        Space complexity: O(1)
+        """
+        for i in range(len(nums)):
+            min_index = i
+            for j in range(i + 1, len(nums)):
+                if nums[j] < nums[min_index]:
+                    min_index = j
+            nums[min_index], nums[i] = nums[i], nums[min_index]
+
+    def sortColors(self, nums: list[int]) -> None:
+        """
+        Time complexity: O(n)
+        Space complexity: O(1)
+        """
+        red = 0
+        white = 0
+        blue = len(nums) - 1
+        # Проходим по всем элементам пока центр не будет рядом с концом
+        while white <= blue:
+            # Если средний равен 0, то меняем местами с левым
+            if nums[white] == 0:
+                nums[white], nums[red] = nums[red], nums[white]
+                red += 1
+                white += 1
+            # Если средний равен 1, то он на месте, увеличиваем индекс
+            elif nums[white] == 1:
+                white += 1
+            # В противном случае средний равен 2, и меняем его с правым
+            else:
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1
 
 
-
-
-nums = [2,0,2,1,1,0]
+nums = [2, 0, 2, 1, 1, 0]
 s = Solution()
 s.sortColors(nums)
 print(nums)
-
-
