80ff5ce3bc0f03ae5d1b8b920920a2efd1e8812d
DESKTOP-5415H2I\thebe <thebestuzl93@gmail.com>
2025-04-17 16:14:10 +0300

    2176[easy] - arrays, 2 solutions

---
+++ 2176_count_equal_and_divisible_pairs_in_an_array.py
@@ -0,0 +1,51 @@
+import itertools
+from collections import defaultdict
+
+
+class Solution:
+    """
+    Grade[Easy]
+
+    Given a 0-indexed integer array nums of length n and an integer k, return the number of pairs (i, j)
+    where 0 <= i < j < n, such that nums[i] == nums[j] and (i * j) is divisible by k.
+
+    Constraints:
+        1 <= nums.length <= 100
+        1 <= nums[i], k <= 100
+    """
+
+    def countPairs(self, nums: list[int], k: int) -> int:
+        """
+        Time Complexity: O(N^2) - Beats 49.67%
+        Space Complexity: O(1) - Beats 32.33%
+        """
+        counter = 0
+        length = len(nums)
+        for i in range(length - 1):
+            for j in range(i + 1, length):
+                if nums[i] == nums[j] and i * j % k == 0:
+                    counter += 1
+        return counter
+
+    def countPairs(self, nums: list[int], k: int) -> int:
+        """
+        Time Complexity: O(N^2) worst case - Beats 95.00%
+        Space Complexity: O(N) - Beats 12.50%
+        """
+        d = defaultdict(list)
+        counter = 0
+        for ind, num in enumerate(nums):
+            d[num].append(ind)
+
+        for indexes in d.values():
+            for pair in list(itertools.combinations(indexes, 2)):
+                if pair[0] * pair[1] % k == 0:
+                    counter += 1
+        return counter
+
+
+nums = [3, 1, 2, 2, 2, 1, 3]
+k = 2
+s = Solution()
+result = s.countPairs(nums, k)
+print(result)
